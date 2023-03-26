d6d9df845f27e80035624946521857d791a11c90
SeptianAdi08 <nugrohoseptian48@gmail.com>
2023-03-27 00:01:42 +0700

    menambahkan

--- src/component/Projek.js
+++ src/component/Projek.js
@@ -9,8 +9,8 @@ const Projek = () => {
         <div className=" w-full px-4">
             <div className=" max-w-xl mx-auto text-center mb-16">
                 <h4>portofolio</h4>
-                <h2>Projek Baru</h2>
-                <p></p>
+                <h2 className=" font-bold tet-dark text-3xl mb-4">Projek Baru</h2>
+                <p>Projek ter</p>
             </div>
         </div>
     </div>
