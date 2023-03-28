3453e70a4408eb61e5ca981fb3a918cfcc0a4a92
SeptianAdi08 <nugrohoseptian48@gmail.com>
2023-03-28 22:45:03 +0700

    menambahkan img portofolio

--- src/component/Projek.js
+++ src/component/Projek.js
@@ -1,4 +1,7 @@
 import React from "react";
+import vegetable from '../styles/img/vegetable.png';
+import portofolio from '../styles/img/portofolio.png';
+
 
 
 
@@ -8,10 +11,27 @@ const Projek = () => {
     <div className="container">
         <div className=" w-full px-4">
             <div className=" max-w-xl mx-auto text-center mb-16">
-                <h4>portofolio</h4>
+                <h4 className=" font-semibold text-lg text-primary mb-2">portofolio</h4>
                 <h2 className=" font-bold tet-dark text-3xl mb-4">Projek Baru</h2>
-                <p>Projek ter</p>
+                <p>Bagian ini akan menampilkan beberapa hasil projek terbaru, untuk melengkapi website portofolio ini.</p>
             </div>
+        </div>
+
+        <div className=" w-full px-4 flex flex-wrap justify-center">
+             <div className=" mb-12 p-4 md:w-1/2">
+                <div className=" rounded-md shadow-md overflow-hidden">
+                    <img src={vegetable} alt="Vegetable png"/>
+                </div>
+                <h3 className=" font-semibold text-dark text-xl mt-5 mb-3">Vegetable Shop</h3>
+                <p className=" font-medium text-base text-secondary">Vegetable Shop ini menampilkan bahan masakan. vegetable shop ini di buat menggunakan html,css dan javascript</p>
+             </div>
+             <div className=" mb-12 p-4 md:w-1/2">
+                <div className=" rounded-md shadow-md overflow-hidden">
+                    <img src={portofolio} alt="Vegetable png"/>
+                </div>
+                <h3 className=" font-semibold text-dark text-xl mt-5 mb-3">Vegetable Shop</h3>
+                <p className=" font-medium text-base text-secondary">Vegetable Shop ini menampilkan bahan masakan. vegetable shop ini di buat menggunakan html,css dan javascript</p>
+             </div>
         </div>
     </div>
     
